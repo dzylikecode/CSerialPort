b0e496f1bc393a75fd4f04706f16a5f692e3c70b
Dz <MathBeWithCode@gmail.com>
2026-05-12 11:30:46 +0800

    option for example and add header

--- bindings/c/xmake.lua
+++ bindings/c/xmake.lua
@@ -8,14 +8,17 @@ target("cserialport")
         add_files("$(projectdir)/lib/version.rc")
     end
     add_includedirs(".", {public = true}) -- cserialport.h
+    add_headerfiles("cserialport.h", {prefixdir = ""})
     add_files("cserialport.cpp")
 
 -- CSerialPort c binding example
-target("CSerialPortDemoC")
-    set_kind("binary")
-    if is_config("CSERIALPORT_ENABLE_NATIVE_SYNC", true) then
-        add_files("example/main_sync_native.c")
-    else
-        add_files("example/main.c")
-    end
-    add_deps("cserialport")
+if is_config("CSERIALPORT_BUILD_EXAMPLES", true) then
+    target("CSerialPortDemoC")
+        set_kind("binary")
+        if is_config("CSERIALPORT_ENABLE_NATIVE_SYNC", true) then
+            add_files("example/main_sync_native.c")
+        else
+            add_files("example/main.c")
+        end
+        add_deps("cserialport")
+end
